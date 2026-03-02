788b9d4bbd8f349d3d4e9f73ae8e5b6790d84b56
Gregory Vincent Jr <gregvjrr@gmail.com>
2026-03-01 22:16:50 -0500

    Added wipe route.

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -79,7 +79,7 @@ const Upload = () => {
             <Navbar/>
             <section className="main-section">
                 <div className={"page-heading py-16"}>
-                    <h1>Smart feedback for your dream back</h1>
+                    <h1>Smart feedback for your dream job</h1>
                     {isProcessing ? (
                         <>
                             <h2>{statusText}</h2>
